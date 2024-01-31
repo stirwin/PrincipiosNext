422dc2aaac717572d036bbe9158d23686c05cef4
Desarrollador Educacion Virtual <desarrolladorvirtual@tecnologicocomfenalco.local>
2024-01-31 17:16:19 -0500

    link

---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <h1>hola mundo</h1>
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+
+export default function Navabar(){
+    return  <ul>
+    <li> <Link href="/">home</Link></li>
+    <li> <Link href="/about">abut</Link></li>
+    <li> <Link href="/tienda">tienda</Link></li>
+  </ul>
+} 
