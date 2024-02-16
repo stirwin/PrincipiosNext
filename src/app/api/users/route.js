5dd58dfa5e9844005630126a95f5ff25e624e392
stirwin <123440202+stirwin@users.noreply.github.com>
2024-02-16 16:45:47 +0000

    router handler

---
+++ src/app/api/users/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+
+export async function GET(){
+    
+    const res = await fecht('https://jsonplaceholder.typicode.com/users');
+    const data= await res.json();
+    return NextResponse.json(data)
+}
+
+export function POST(){
+    return NextResponse.json({
+        menssage:"creando"
+    })
+}
+
+export function PUT(){
+    return NextResponse.json({
+        menssage:"Actualizando"
+    })
+}
+
+export function DELETE(){
+    return NextResponse.json({
+        menssage:"ELIMINANDO"
+    })
+}
